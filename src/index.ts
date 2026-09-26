17d124187ef755368bc233d54c5af13efba10814
Suronjit Sutradhar <suronjit02@gmail.com>
2026-09-26 19:59:21 +0600

    fix: change origin url

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://poster-maker-frontend.vercel.app",
     credentials: true,
   }),
 );
