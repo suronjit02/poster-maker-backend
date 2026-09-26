6c40ba774546e718a4ac7db959622cbbacdf8d3c
Suronjit Sutradhar <suronjit02@gmail.com>
2026-09-26 06:00:32 +0600

    fix: add auth protection to upload endpoint

--- src/routes/uploadRoutes.ts
+++ src/routes/uploadRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import upload from "../middlewares/multerMiddleware.js";
 import { uploadPhoto } from "../controllers/uploadController.js";
+import { protect } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
-router.post("/", upload.single("photo"), uploadPhoto);
+router.post("/", protect, upload.single("photo"), uploadPhoto);
 
 export default router;
